3c1325d838e35754822168507f010fe84b2eb94d
kitou88C39 <kanfutrooper@icloud.com>
2023-04-30 16:46:01 +0900

    Add.tsx 編集

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-
-function App() {
+import { VStack } from '@chakra-ui/layout';
+import Top from './views/pages/Top';
+import Login from './views/components/auth/Login';
+import Main from './views/pages/Main';
+import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+const App: React.FC = () => {
+  const [isLogin, setIsLogin] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <VStack spacing={4} align='stretch' p={0}>
+      <Routes>
+        <Route path='/' element={<Top />} />
+        <Route path='/Login' element={<Login isLogin={isLogin} />} />
+        <Route path='/Main' element={<Main />} />
+      </Routes>
+    </VStack>
   );
-}
+};
 
 export default App;
